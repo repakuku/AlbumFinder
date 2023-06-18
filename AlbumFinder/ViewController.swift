b7e045461db274d9700e644fa9ac6c0d27396849
Alexey Turulin <repakuku@icloud.com>
2023-06-18 18:25:08 -0400

    Initial Commit

---
+++ AlbumFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumFinder
+//
+//  Created by Алексей Турулин on 6/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
